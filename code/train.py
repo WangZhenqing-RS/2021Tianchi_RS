a5cc65b741f16d743178235a0a7903ed851c4384
WangZhenqing <41787821+WangZhenqing-RS@users.noreply.github.com>
2021-03-03 09:58:19 +0800

    Update train.py

--- code/train.py
+++ code/train.py
@@ -15,7 +15,9 @@ import segmentation_models_pytorch as smp
 import glob
 from segmentation_models_pytorch.losses import DiceLoss, SoftCrossEntropyLoss, LovaszLoss
 from pytorch_toolbelt import losses as L
+# from model import seg_hrnet_ocr
 from torch.optim.swa_utils import AveragedModel, SWALR
+
 ## 使用自动混合精度训练，在尽可能减少精度损失的情况下利用半精度浮点数加速训练
 #from torch.cuda.amp import autocast, GradScaler
 
@@ -32,7 +34,7 @@ DEVICE = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
 def train(EPOCHES, BATCH_SIZE, train_image_paths, train_label_paths, 
           val_image_paths, val_label_paths, channels, optimizer_name,
-          model_path, swa_model_path, addNDVI, loss):
+          model_path, swa_model_path, addNDVI, loss, early_stop):
     
     train_loader = get_dataloader(train_image_paths, train_label_paths, 
                                   "train", addNDVI, BATCH_SIZE, shuffle=True, num_workers=8)
@@ -40,14 +42,21 @@ def train(EPOCHES, BATCH_SIZE, train_image_paths, train_label_paths,
                                   "val", addNDVI, BATCH_SIZE, shuffle=False, num_workers=8)
     
     # 定义模型,优化器,损失函数
+    # model = smp.UnetPlusPlus(
+    #         encoder_name="efficientnet-b7",
+    #         encoder_weights="imagenet",
+    #         in_channels=channels,
+    #         classes=10,
+    # )
     model = smp.UnetPlusPlus(
-            encoder_name="efficientnet-b7",
+            encoder_name="timm-resnest101e",
             encoder_weights="imagenet",
             in_channels=channels,
             classes=10,
     )
+    # model = seg_hrnet_ocr.get_seg_model()
     model.to(DEVICE);
-    
+    # model.load_state_dict(torch.load(model_path))
     # 采用SGD优化器
     if(optimizer_name == "sgd"):
         optimizer = torch.optim.SGD(model.parameters(), 
@@ -63,13 +72,12 @@ def train(EPOCHES, BATCH_SIZE, train_image_paths, train_label_paths,
             T_mult=2, # T_mult就是重启之后因子,即每个restart后，T_0 = T_0 * T_mult
             eta_min=1e-5 # 最低学习率
             ) 
-    # 使用SWA的初始epoch,一般是EPOCHES的0.75倍
-    swa_start = int(EPOCHES * 0.75)
-    # 随机权重平均SWA,以几乎不增加任何成本的方式实现更好的泛化
-    swa_model = AveragedModel(model).to(DEVICE)
-    # SWA调整学习率
-    swa_scheduler = SWALR(optimizer, swa_lr=1e-5)
-    # loss_fn = nn.CrossEntropyLoss().to(DEVICE);
+    # # 使用SWA的初始epoch
+    # swa_start = 80
+    # # 随机权重平均SWA,以几乎不增加任何成本的方式实现更好的泛化
+    # swa_model = AveragedModel(model).to(DEVICE)
+    # # SWA调整学习率
+    # swa_scheduler = SWALR(optimizer, swa_lr=1e-5)
     
     if(loss == "SoftCE_dice"):
         # 损失函数采用SoftCrossEntropyLoss+DiceLoss
@@ -87,6 +95,115 @@ def train(EPOCHES, BATCH_SIZE, train_image_paths, train_label_paths,
         SoftCrossEntropy_fn=SoftCrossEntropyLoss(smooth_factor=0.1)
         loss_fn = L.JointLoss(first=LovaszLoss_fn, second=SoftCrossEntropy_fn,
                               first_weight=0.5, second_weight=0.5).cuda()
+    
+    header = r'Epoch/EpochNum | TrainLoss | ValidmIoU | Time(m)'
+    raw_line = r'{:5d}/{:8d} | {:9.3f} | {:9.3f} | {:9.2f}'
+    print(header)
+    
+#    # 在训练最开始之前实例化一个GradScaler对象,使用autocast才需要
+#    scaler = GradScaler()
+
+    # 记录当前验证集最优mIoU,以判定是否保存当前模型
+    best_miou = 0
+    best_miou_epoch = 0
+    train_loss_epochs, val_mIoU_epochs, lr_epochs = [], [], []
+    # 开始训练
+    for epoch in range(1, EPOCHES+1):
+        # print("Start training the {}st epoch...".format(epoch))
+        # 存储训练集每个batch的loss
+        losses = []
+        start_time = time.time()
+        model.train()
+        model.to(DEVICE);
+        for batch_index, (image, target) in enumerate(train_loader):
+            image, target = image.to(DEVICE), target.to(DEVICE)
+            # 在反向传播前要手动将梯度清零
+            optimizer.zero_grad()
+#            # 使用autocast半精度加速训练,前向过程(model + loss)开启autocast
+#            with autocast(): #need pytorch>1.6
+            # 模型推理得到输出
+            output = model(image)
+            # 求解该batch的loss
+            loss = loss_fn(output, target)
+#                scaler.scale(loss).backward()
+#                scaler.step(optimizer)
+#                scaler.update()
+            # 反向传播求解梯度
+            loss.backward()
+            # 更新权重参数
+            optimizer.step()
+            losses.append(loss.item())
+        # if epoch > swa_start:
+        #     swa_model.update_parameters(model)
+        #     swa_scheduler.step()
+        # else:
+            # 余弦退火调整学习率
+        scheduler.step()
+        # 计算验证集IoU
+        val_iou = cal_val_iou(model, valid_loader)
+        # 输出验证集每类IoU
+        # print('\t'.join(np.stack(val_iou).mean(0).round(3).astype(str)))
+        # 保存当前epoch的train_loss.val_mIoU.lr_epochs
+        train_loss_epochs.append(np.array(losses).mean())
+        val_mIoU_epochs.append(np.mean(val_iou))
+        lr_epochs.append(optimizer.param_groups[0]['lr'])
+        # 输出进程
+        print(raw_line.format(epoch, EPOCHES, np.array(losses).mean(), 
+                              np.mean(val_iou), 
+                              (time.time()-start_time)/60**1), end="")    
+        if best_miou < np.stack(val_iou).mean(0).mean():
+            best_miou = np.stack(val_iou).mean(0).mean()
+            best_miou_epoch = epoch
+            torch.save(model.state_dict(), model_path)
+            print("  valid mIoU is improved. the model is saved.")
+        else:
+            print("")
+            if (epoch - best_miou_epoch) >= early_stop:
+                break
+    # # 最后更新BN层参数
+    # torch.optim.swa_utils.update_bn(train_loader, swa_model, device= DEVICE)
+    # # 计算验证集IoU
+    # val_iou = cal_val_iou(model, valid_loader)
+    # print("swa_model'mIoU is {}".format(np.mean(val_iou)))
+    # torch.save(swa_model.state_dict(), swa_model_path)
+    return train_loss_epochs, val_mIoU_epochs, lr_epochs
+
+
+def fine_tune(EPOCHES, BATCH_SIZE, train_image_paths, train_label_paths, 
+              val_image_paths, val_label_paths, channels, model_path,
+              swa_model_path, addNDVI):
+    
+    train_loader = get_dataloader(train_image_paths, train_label_paths, 
+                                  "train", addNDVI, BATCH_SIZE, shuffle=True, num_workers=8)
+    valid_loader = get_dataloader(val_image_paths, val_label_paths, 
+                                  "val", addNDVI, BATCH_SIZE, shuffle=False, num_workers=8)
+    
+    # 定义模型,优化器,损失函数
+    # model = smp.UnetPlusPlus(
+    #         encoder_name="efficientnet-b7",
+    #         encoder_weights="imagenet",
+    #         in_channels=channels,
+    #         classes=10,
+    # )
+    model = smp.UnetPlusPlus(
+            encoder_name="resnet101",
+            encoder_weights="imagenet",
+            in_channels=channels,
+            classes=10,
+    )
+    model.to(DEVICE);
+    model.load_state_dict(torch.load(model_path))
+    # 采用SGD优化器
+    optimizer = torch.optim.SGD(model.parameters(), 
+                                lr=3e-4, weight_decay=1e-3, momentum=0.9)
+
+    # 随机权重平均SWA,实现更好的泛化
+    swa_model = AveragedModel(model).to(DEVICE)
+    # SWA调整学习率
+    swa_scheduler = SWALR(optimizer, swa_lr=1e-5)
+
+    # LovaszLoss是对基于子模块损失凸Lovasz扩展的mIoU损失的直接优化
+    loss_fn = LovaszLoss(mode='multiclass').to(DEVICE)
     
     header = r'Epoch/EpochNum | TrainLoss | ValidmIoU | Time(m)'
     raw_line = r'{:5d}/{:8d} | {:9.3f} | {:9.3f} | {:9.2f}'
@@ -124,12 +241,8 @@ def train(EPOCHES, BATCH_SIZE, train_image_paths, train_label_paths,
             # 更新权重参数
             optimizer.step()
             losses.append(loss.item())
-        if epoch > swa_start:
-            swa_model.update_parameters(model)
-            swa_scheduler.step()
-        else:
-            # 余弦退火调整学习率
-            scheduler.step()
+        swa_model.update_parameters(model)
+        swa_scheduler.step()
         # 计算验证集IoU
         val_iou = cal_val_iou(model, valid_loader)
         # 输出验证集每类IoU
@@ -144,7 +257,7 @@ def train(EPOCHES, BATCH_SIZE, train_image_paths, train_label_paths,
                               (time.time()-start_time)/60**1), end="")    
         if best_miou < np.stack(val_iou).mean(0).mean():
             best_miou = np.stack(val_iou).mean(0).mean()
-            torch.save(model.state_dict(), model_path)
+            torch.save(model.state_dict(), model_path[:-4] + "_finetune.pth")
             print("  valid mIoU is improved. the model is saved.")
         else:
             print("")
@@ -155,31 +268,36 @@ def train(EPOCHES, BATCH_SIZE, train_image_paths, train_label_paths,
     print("swa_model'mIoU is {}".format(np.mean(val_iou)))
     torch.save(swa_model.state_dict(), swa_model_path)
     return train_loss_epochs, val_mIoU_epochs, lr_epochs
-
+    
 # 不加主函数这句话的话,Dataloader多线程加载数据会报错
 if __name__ == '__main__':
-    EPOCHES = 80
+    EPOCHES = 40
     BATCH_SIZE = 16
     image_paths = glob.glob(r'..\tcdata\suichang_round1_train_210120\*.tif')
     label_paths = glob.glob(r'..\tcdata\suichang_round1_train_210120\*.png')
     # 每5个数据的第val_index个数据为验证集
     val_index = 0
+    upsample = True
     train_image_paths, train_label_paths, val_image_paths, val_label_paths = split_train_val(image_paths, 
                                                                                              label_paths, 
-                                                                                             val_index)
+                                                                                             val_index, 
+                                                                                             upsample)
     loss = "SoftCE_dice"
     #loss = "SoftCE_Lovasz"
     channels = 4
-    addNDVI = True
-    if(addNDVI):
+    addNDVI = False
+    if(addNDVI): 
         channels += 1
     optimizer_name = "adamw"
-    model_path = "../user_data/model_data/unetpp_effi7"
+    model_path = "../user_data/model_data/unetplusplus_dpn131"
+    if(upsample):
+        model_path += "_upsample"
     if(addNDVI):
         model_path += "_ndvi"
     model_path += "_" + loss
     swa_model_path = model_path + "_swa.pth"
     model_path += ".pth"
+    early_stop = 6
     train_loss_epochs, val_mIoU_epochs, lr_epochs = train(EPOCHES, 
                                                           BATCH_SIZE, 
                                                           train_image_paths, 
@@ -191,25 +309,31 @@ if __name__ == '__main__':
                                                           model_path, 
                                                           swa_model_path, 
                                                           addNDVI,
-                                                          loss)
+                                                          loss,
+                                                          early_stop)
+    
+    # EPOCHES = 10
+    # train_loss_epochs, val_mIoU_epochs, lr_epochs = fine_tune(EPOCHES,
+    #                                                           BATCH_SIZE,
+    #                                                           train_image_paths,
+    #                                                           train_label_paths,
+    #                                                           val_image_paths,
+    #                                                           val_label_paths,
+    #                                                           channels,
+    #                                                           model_path,
+    #                                                           swa_model_path,
+    #                                                           addNDVI)
     if(True):    
         import matplotlib.pyplot as plt
-        x = [i for i in range(EPOCHES)]
-        fig = plt.figure(figsize=(12, 4))
-        ax = fig.add_subplot(1, 2, 1)
-        ax.plot(x, train_loss_epochs, label='train loss')
-        ax.plot(x, val_mIoU_epochs, label='val mIoU')
-        ax.set_xlabel('epoch', fontsize=15)
-        ax.set_ylabel('metrics', fontsize=15)
-        ax.set_title('train curve', fontsize=15)
-        ax.grid(True)
-        plt.legend(loc='upper right', fontsize=15)
-        ax = fig.add_subplot(1, 2, 2)
-        ax.plot(x, lr_epochs,  label='learning rate')
-        ax.set_xlabel('epoch', fontsize=15)
-        ax.set_ylabel('learning rate', fontsize=15)
-        ax.set_title('lr curve', fontsize=15)
-        ax.grid(True)
-        plt.legend(loc='upper right', fontsize=15)
-        plt.savefig("train curve", dpi=300)
-        plt.show()
+        epochs = range(1, len(train_loss_epochs) + 1)
+        plt.plot(epochs, train_loss_epochs, 'r', label = 'train loss')
+        plt.plot(epochs, val_mIoU_epochs, 'b', label = 'val mIoU')
+        plt.title('train loss and val mIoU')
+        plt.legend()
+        plt.savefig("train loss and val mIoU.png",dpi = 300)
+        plt.figure()
+        plt.plot(epochs, lr_epochs, 'r', label = 'learning rate')
+        plt.title('learning rate')
+        plt.legend()
+        plt.savefig("learning rate.png", dpi = 300)
+        plt.show() 
